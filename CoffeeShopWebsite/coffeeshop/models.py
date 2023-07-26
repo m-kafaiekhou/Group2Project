f2c2f5b6fbce40e55d4a9b9d134acb9f29be0974
Arad <aradarvinxo@gmail.com>
2023-07-26 12:33:57 +0330

    chore:model for order & order item & cafeitem made

--- CoffeeShopWebsite/coffeeshop/models.py
+++ CoffeeShopWebsite/coffeeshop/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class Order(models.Model):
+    phone_number = PhoneNumberField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    table_number = models.IntegerField(max_length=2, default=None)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class OrderItem(models.Model):
+    quantity = models.IntegerField(max_length=4)
+
+
+class CafeItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="cafe-item/", blank=True, null=True)
+    is_available = models.BooleanField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class Review(models.Model):
+    pass
+
+
+class SubCategory(models.Model):
+    pass
+
+
+class ParentCategory(models.Model):
+    pass
