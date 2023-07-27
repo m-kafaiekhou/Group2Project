93f6ebe6f019748b8a71c36cf8667fe2e3d30933
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-27 18:10:45 +0330

    chore: CustomUserManager/create_superuser

--- CoffeeShopWebsite/staff/managers.py
+++ CoffeeShopWebsite/staff/managers.py
@@ -12,3 +12,17 @@ class CustomUserManager(BaseUserManager):
         user.save()
 
         return user
+
+    def create_superuser(self, phone_number, first_name, last_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(phone_number, first_name, last_name, password, **other_fields)
