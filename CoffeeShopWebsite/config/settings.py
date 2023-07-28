d35db0d3767e0168c8ed6b8669195f1ecfc96cdd
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-28 14:28:29 +0330

    refactor: installed app list

--- CoffeeShopWebsite/config/settings.py
+++ CoffeeShopWebsite/config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.staff.StaffConfig",
-    "django.coffeeshop.CoffeeshopConfig",
+    'staff',
+    "coffeeshop",
 ]
 
 MIDDLEWARE = [
