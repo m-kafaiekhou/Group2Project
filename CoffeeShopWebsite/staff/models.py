2a433118c840831e45d1fac142bd83b6566828b7
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-28 22:59:26 +0330

    refactor: cleaned field code

--- CoffeeShopWebsite/staff/models.py
+++ CoffeeShopWebsite/staff/models.py
@@ -8,7 +8,10 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     _REGEX = r'09(\d{9})$'
     phone_validator = RegexValidator(_REGEX, "The phone number provided is invalid")
 
-    phone_number = models.CharField('phone number', max_length=16, validators=[phone_validator], unique=True, null=False)
+    phone_number = models.CharField(
+        'phone number', max_length=16, validators=[phone_validator],
+        unique=True, null=False
+    )
     last_name = models.CharField(max_length=150, blank=True, null=True)
     first_name = models.CharField(max_length=150, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
