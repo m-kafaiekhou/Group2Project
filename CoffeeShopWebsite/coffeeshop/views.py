76852a8eb125611a39e6cdb9d7ee52404b76c386
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-30 08:44:18 +0330

    chore: cart view

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+# class CartView(generic.DetailView):
+def cart_view(request):
+    return render(request, 'coffeeshop/cart.html')
