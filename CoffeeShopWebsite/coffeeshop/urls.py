22ce8a7965aa043ff384a92a8250ffd68bbfd7e1
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-07-30 08:54:56 +0330

    chore: checkout template

--- CoffeeShopWebsite/coffeeshop/urls.py
+++ CoffeeShopWebsite/coffeeshop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import cart_view
+from .views import cart_view, checkout_view
 
 urlpatterns = [
     path("cart/", cart_view, name='cart'),
+    path("cart/", checkout_view, name='checkout'),
 ]
--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views import generic
 
 # Create your views here.
 
-# class CartView(generic.DetailView):
+
 def cart_view(request):
     return render(request, 'coffeeshop/cart.html')
+
+
+def checkout_view(request):
+    return render(request, 'coffeeshop/checkout.html')
