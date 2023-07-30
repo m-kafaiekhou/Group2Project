4abb97ece512fa60263f4180f8eb9ce9072f9fdf
Arad <aradarvinxo@gmail.com>
2023-07-31 00:46:04 +0330

    chore: add to cart function done. needs rework.

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def add_to_cart(response, value: list[dict(str, int)]) -> None:
+    """
+    This function sets a cookie and follows cart behavior.
+
+    value is a list of dictionaries:
+    [{menu_item:quantity}, ...]
+    """
+    max_age = 7 * 24 * 60 * 60
+    response.set_cookie("cart", value, max_age=max_age)
