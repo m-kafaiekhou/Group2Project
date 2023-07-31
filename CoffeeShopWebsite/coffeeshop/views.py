95ad687f85d0fc6a2ddfc88755baaa9b687b3f9e
arta16h <sevda.hayati2015@gmail.com>
2023-07-31 19:58:05 +0330

    Chore: searching the menu

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import CafeItem
+from .search import SearchMenu
 
 # Create your views here.
-def menu(request):
-    return render(request, 'coffeeshop/menu.html')
+def menu(request) :
+    cafeitem = CafeItem.objects.all()
+    form = SearchMenu()
+    if 'search' in request.GET:
+        form = SearchMenu(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data['search']
+            cafeitem = cafeitem.filter(Q(name__icontains=cd) | Q(description__icontains=cd))
+    return render(request, 'menu/menu.html', {'cafeitem':cafeitem, 'form' : form})
