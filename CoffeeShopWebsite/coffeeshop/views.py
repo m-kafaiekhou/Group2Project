2ee92ce4551737a55e777f863f36e943b62f2e1e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-31 20:07:49 +0330

    refactor: change class based view to function based view

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.views import View
+
+# from django.views import View
 
 
-class HomePageView(View):
-    def get(self, request):
-        # a class method should written to get top rated items
-        top_rated_items = []
-        return render(
-            request, "coffeeshop/home.html", {"top_rated_items": top_rated_items}
-        )
+# class HomePageView(View):
+#     def get(self, request):
+#         # a class method should written to get top rated items
+#         top_rated_items = []
+#         return render(
+#             request, "coffeeshop/home.html", {"top_rated_items": top_rated_items}
+#         )
+
+
+def hemo_page(request):
+    top_rated_items = []
+    return render(request, "coffeeshop/home.html", {"top_rated_items": top_rated_items})
