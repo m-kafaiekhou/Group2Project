448300bcab92abe8674eea1a9f9371d3175afe9e
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-02 18:45:26 +0330

    chore: add humanize app

--- CoffeeShopWebsite/config/settings.py
+++ CoffeeShopWebsite/config/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my apps
     "staff",
     "coffeeshop",
+    # third party apps
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
