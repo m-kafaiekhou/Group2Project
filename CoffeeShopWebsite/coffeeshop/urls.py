db2fed0ec08330a0a5d0ea58ffb9390bc6599aae
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-02 22:22:30 +0330

    fix: views in url fixed

--- CoffeeShopWebsite/coffeeshop/urls.py
+++ CoffeeShopWebsite/coffeeshop/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cart/", cart_view, name='cart'),
-    path("checkout/", checkout_view, name='checkout'),
+    path("cart/", views.cart_view, name='cart'),
+    path("checkout/", views.checkout_view, name='checkout'),
     path("menu/", views.menu, name="menu"),
     path("menu/search", views.menu_search, name='menu_search'),
-    path("home/", views.hemo_page, name="home"),
+    path("", views.home_page, name="home"),
 ]
