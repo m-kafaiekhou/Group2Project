33fb164aed2b4523b30b13aa6e1a9f2a77584133
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-02 18:46:39 +0330

    chore: cart and checkout view

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -7,24 +7,32 @@ from .models import CafeItem
 
 def cart_view(request):
     request.COOKIES['cart'] = '[{"1":2}, {"2":3}]'
+    cart, total = get_cart(request)
+    if cart:
+        return render(request, 'coffeeshop/cart.html', context={'items': cart, 'total': total})
+    else:
+        return redirect('checkout')
+
+
+def get_cart(request):
     cart = request.COOKIES.get('cart', None)
     if cart:
         items = json.loads(cart)
         object_lst = [CafeItem.objects.get(pk=pk) for item in items for pk in item.keys()]
         quantity_lst = [q for item in items for q in item.values()]
-        items = {obj: quant for (obj, quant) in zip(object_lst, quantity_lst)}
-        response = render(request, 'coffeeshop/cart.html', context={'items': items})
-        response.set_cookie('cart', '[{"1":2}, {"2":3}]')
+        items = {}
+        total = 0
+        for obj, quant in zip(object_lst, quantity_lst):
+            items[obj] = quant
+            total += obj.price * quant
+        return items, total
 
-        return response
-    else:
-        return redirect('checkout')
+    return None
 
 
 def checkout_view(request):
-    cart = request.COOKIES.get('cart', None)
+    cart, total = get_cart(request)
     if cart:
-        pass
+        return render(request, 'coffeeshop/checkout.html', context={'items': cart, 'total': total})
     else:
         return redirect('login')
-    return render(request, 'coffeeshop/checkout.html')
