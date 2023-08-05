a4f9970d8936fefc459e1cff596f3cab7c595583
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 20:52:13 +0330

    refactor: rm urls

--- CoffeeShopWebsite/coffeeshop/urls.py
+++ CoffeeShopWebsite/coffeeshop/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cart/", views.cart_view, name='cart'),
-    path("checkout/", views.checkout_view, name='checkout'),
-    path("menu/", views.menu, name="menu"),
-    path("menu/search/", views.menu_search, name='menu_search'),
-    path("delete-cart/", views.delete_cart_view, name='delete_cart'),
     path("", views.home_page, name="home"),
 ]
