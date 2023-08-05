76a0c468b5381f3f6b33d5e106ca6758859526b8
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 21:30:17 +0330

    refactor: rm useless imports

--- CoffeeShopWebsite/coffeeshop/admin.py
+++ CoffeeShopWebsite/coffeeshop/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import Order, OrderItem, CafeItem, Review, Category
+from .models import Review
 
 # Register your models here.
 
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(CafeItem)
 admin.site.register(Review)
-admin.site.register(Category)
