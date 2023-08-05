881e1d559937691ce1f3ccbd44af4eab1271e476
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 20:57:51 +0330

    refactor: add urls

---
+++ CoffeeShopWebsite/orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path("cart/", views.cart_view, name='cart'),
+    path("checkout/", views.checkout_view, name='checkout'),
+    path("delete-cart/", views.delete_cart_view, name='delete_cart'),
+]
