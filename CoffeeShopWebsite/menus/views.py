34ae046ddbd2a0dbad7a847041c81db1b5d95531
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 20:08:47 +0330

    refactor: moved model from views to model

--- CoffeeShopWebsite/menus/views.py
+++ CoffeeShopWebsite/menus/views.py
@@ -39,33 +39,3 @@ def menu(request):
     return response
 
 
-class CafeItem(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=150)
-    image = models.ImageField(upload_to="cafe_item/", blank=True, null=True)
-    is_available = models.BooleanField()
-    price = models.IntegerField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    category = models.ForeignKey(
-        "Category",
-        on_delete=models.PROTECT,
-    )
-
-    @property
-    def item_rate(self):
-        reviews = self.review_set
-        rates = [rev.rating for rev in reviews]
-        return sum(rates) / len(rates)
-
-    @classmethod
-    def top_rated_items(cls):
-        return CafeItem.objects.annotate(item_rate=Avg("review_set__rating")).order_by(
-            "-item_rate"
-        )[:3]
-
-    def category_name(self):
-        return self.category.parent_category
-
-    def __str__(self) -> str:
-        return self.name
