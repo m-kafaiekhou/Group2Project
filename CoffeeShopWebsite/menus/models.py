3a4d1e494d7eb9428ca819f6390a1cb43fb9d46c
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 19:54:53 +0330

    refactor: moved models

--- CoffeeShopWebsite/menus/models.py
+++ CoffeeShopWebsite/menus/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    parent_category = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="category", blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
