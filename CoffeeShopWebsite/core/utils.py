10807a55978ebda688a77abb9f044da0939f762d
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 20:19:06 +0330

    refactor: rm imports

--- CoffeeShopWebsite/core/utils.py
+++ CoffeeShopWebsite/core/utils.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import CafeItem, Order, OrderItem, Review, Category
+
 
 
 def add_to_cart(request, response, item_pk: int) -> None:
