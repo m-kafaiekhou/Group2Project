4297933d783aa9a07de9af30fa99e6276557f0bb
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-05 20:08:18 +0330

    refactor: add avg

--- CoffeeShopWebsite/menus/models.py
+++ CoffeeShopWebsite/menus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg
 
 # Create your models here.
 
@@ -11,6 +12,38 @@ class Category(models.Model):
     )
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to="category", blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CafeItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="cafe_item/", blank=True, null=True)
+    is_available = models.BooleanField()
+    price = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.PROTECT,
+    )
+
+    @property
+    def item_rate(self):
+        reviews = self.review_set
+        rates = [rev.rating for rev in reviews]
+        return sum(rates) / len(rates)
+
+    @classmethod
+    def top_rated_items(cls):
+        return CafeItem.objects.annotate(item_rate=Avg("review_set__rating")).order_by(
+            "-item_rate"
+        )[:3]
+
+    def category_name(self):
+        return self.category.parent_category
 
     def __str__(self) -> str:
         return self.name
