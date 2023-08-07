b16e752f6553a2e67fb1a3f00289e9409ce54741
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-08 01:38:24 +0330

    refactor: clean

--- CoffeeShopWebsite/orders/views.py
+++ CoffeeShopWebsite/orders/views.py
@@ -69,36 +69,6 @@ class CheckoutView(View):
         create_session(request, phone_number=phone_number, order_id=order.id)
         return redirect("home")
 
-# def checkout_view(request):
-#     cart, total = get_cart(request)
-#     if request.method == "POST":
-#         phone_number = request.POST.get("phone_number")
-#         table_number = request.POST.get("table_number")
-#         order = Order.objects.create(
-#             phone_number=phone_number,
-#             table_number=table_number,
-#             total_price=total,
-#             status="d",
-#         )
-#         object_lst = [obj for obj, _ in cart.items()]
-#         quantity_lst = [val for _, val in cart.items()]
-#         zipped = zip(object_lst, quantity_lst)
-#         for item, quant in zipped:
-#             OrderItem.objects.create(order_fk=order, cafeitem_fk=item, quantity=quant)
-#
-#         create_session(request, phone_number=phone_number, order_id=order.id)
-#         return redirect("home")
-#
-#     else:
-#         if cart:
-#             return render(
-#                 request,
-#                 "orders/checkout.html",
-#                 context={"items": cart, "total": total},
-#             )
-#         else:
-#             return redirect("menu")
-
 
 def delete_cart_view(request):
     response = redirect("cart")
