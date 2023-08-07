54a440018bdd7996bc063d8867641f315298d177
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-07 04:05:15 +0330

    chore: view as view

--- CoffeeShopWebsite/orders/urls.py
+++ CoffeeShopWebsite/orders/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("cart/", views.cart_view, name='cart'),
+    path("cart/", views.CartView.as_view(), name='cart'),
     path("checkout/", views.checkout_view, name='checkout'),
     path("delete-cart/", views.delete_cart_view, name='delete_cart'),
 ]
