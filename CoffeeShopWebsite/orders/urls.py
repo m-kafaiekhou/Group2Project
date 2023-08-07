ddebe0ecf02036fefd30714a74dc7635c846b976
arta16h <sevda.hayati2015@gmail.com>
2023-08-07 05:11:50 +0330

    Refactor: class based view-CheckoutView

--- CoffeeShopWebsite/orders/urls.py
+++ CoffeeShopWebsite/orders/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("cart/", views.CartView.as_view(), name='cart'),
-    path("checkout/", views.checkout_view, name='checkout'),
+    path("checkout/", views.CheckoutView.as_view(), name='checkout'),
     path("delete-cart/", views.delete_cart_view, name='delete_cart'),
 ]
--- CoffeeShopWebsite/orders/views.py
+++ CoffeeShopWebsite/orders/views.py
@@ -39,38 +39,40 @@ class GetCart(View) :
         return None, None
 
 
-def checkout_view(request):
-    cart, total = get_cart(request)
-    if request.method == "POST":
-        phone_number = request.POST.get("phone_number")
-        table_number = request.POST.get("table_number")
-        order = Order.objects.create(
-            phone_number=phone_number,
-            table_number=table_number,
-            total_price=total,
-            status="d",
-        )
-        object_lst = [obj for obj, _ in cart.items()]
-        quantity_lst = [val for _, val in cart.items()]
-        zipped = zip(object_lst, quantity_lst)
-        for item, quant in zipped:
-            OrderItem.objects.create(order_fk=order, cafeitem_fk=item, quantity=quant)
-
-        create_session(request, phone_number=phone_number, order_id=order.id)
-        return redirect("home")
-
-    else:
-        if cart:
-            return render(
-                request,
-                "orders/checkout.html",
-                context={"items": cart, "total": total},
+class CheckoutView(View) :
+    def post(request) :
+        cart, total = GetCart(request)
+        if request.method == "POST":
+            phone_number = request.POST.get("phone_number")
+            table_number = request.POST.get("table_number")
+            order = Order.objects.create(
+                phone_number=phone_number,
+                table_number=table_number,
+                total_price=total,
+                status="d",
             )
+            object_lst = [obj for obj, _ in cart.items()]
+            quantity_lst = [val for _, val in cart.items()]
+            zipped = zip(object_lst, quantity_lst)
+            for item, quant in zipped:
+                OrderItem.objects.create(order_fk=order, cafeitem_fk=item, quantity=quant)
+
+            create_session(request, phone_number=phone_number, order_id=order.id)
+            return redirect("home")
+
         else:
-            return redirect("menu")
+            if cart:
+                return render(
+                    request,
+                    "orders/checkout.html",
+                    context={"items": cart, "total": total},
+                )
+            else:
+                return redirect("menu")
 
 
 def delete_cart_view(request):
     response = redirect("cart")
     delete_cart(request, response)
     return response
+    
