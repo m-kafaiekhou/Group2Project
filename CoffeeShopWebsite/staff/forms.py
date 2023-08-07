e386e945c787bbb7f9b4df5e2a9bbb225a68d428
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-07 20:32:54 +0330

    chore: import forms

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -1,5 +1,6 @@
 # from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
-# from django import forms
+from django import forms
+
 #
 #
 # class CustomAuthenticationForm(AuthenticationForm):
