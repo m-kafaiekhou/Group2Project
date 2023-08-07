6e0648a5ec38e7c132687ddf70e91daff36ffc13
Arad <aradarvinxo@gmail.com>
2023-08-07 17:27:02 +0330

    refactor: menu items img context for gallery page

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from menus.models import CafeItem
 # Create your views here.
 
 
 class HomePage(View):
+    template_name = "coffeeshop/home.html"
 
     def get(self, request, *args, **kwargs):
         # top_rated_items = CafeItem.top_rated_items()
-        return render(request, "coffeeshop/home.html", {"top_rated_items": None})
+        return render(request, self.template_name, {"top_rated_items": None})
 
 
 class GalleryPage(View):
+    template_name = "coffeeshop/gallery.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, "coffeeshop/gallery.html")
+        items = CafeItem.objects.all()
+        context = {"items":items}
+        return render(request, self.template_name, context)
