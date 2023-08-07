33b1781c3eab03e714a92ee4a68b9e724d943032
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-07 21:09:07 +0330

    chore: define phone validator using regex

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -13,6 +13,7 @@ from django.core.validators import RegexValidator
 
 class CustomUserLoginForm(forms.Form):
     _REGEX = r"09(\d{9})$"
+    phone_validator = RegexValidator(_REGEX, "The phone number provided is invalid")
 
     phone_number = forms.CharField(max_length=14)
 
