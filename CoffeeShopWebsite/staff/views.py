5c47848bbd9db464e6ab524f28bd830c95bf3788
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-07 21:23:17 +0330

    chore: import CustomUserLoginForm

--- CoffeeShopWebsite/staff/views.py
+++ CoffeeShopWebsite/staff/views.py
@@ -1,6 +1,7 @@
 # from django.contrib.auth.views import LoginView
 # from .forms import CustomAuthenticationForm
 from django.views import View
+from .forms import CustomUserLoginForm
 
 #
 # class CustomLoginView(LoginView):
