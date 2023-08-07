2b43cdb5b71f8f30e22743239431f2b15f56f89c
Arad <aradarvinxo@gmail.com>
2023-08-07 13:24:57 +0330

    refactor: FBV change to CBV. HomePage

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
 
 
-def home_page(request):
-    # top_rated_items = CafeItem.top_rated_items()
-    return render(request, "coffeeshop/home.html", {"top_rated_items": None})
+class HomePage(View):
+
+    def get(self, request, *args, **kwargs):
+        # top_rated_items = CafeItem.top_rated_items()
+        return render(request, "coffeeshop/home.html", {"top_rated_items": None})
