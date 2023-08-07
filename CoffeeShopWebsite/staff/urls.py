80141d33f0663ce4c279e19358e206f86d012480
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-07 23:05:41 +0330

    chore: define a url for verifying

--- CoffeeShopWebsite/staff/urls.py
+++ CoffeeShopWebsite/staff/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CustomUserLoginView
+from .views import CustomUserLoginView, CustomUserLoginVerifyView
 
 urlpatterns = [
     path("login/", CustomUserLoginView.as_view(), name="login"),
+    path("verify", CustomUserLoginVerifyView.as_view(), name="verify"),
 ]
