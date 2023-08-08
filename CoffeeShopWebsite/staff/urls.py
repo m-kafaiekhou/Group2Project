67aba0e5df55591d879b4ffa4986af6c497c94ba
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 12:31:20 +0330

    refactor: remove urls related on otp

--- CoffeeShopWebsite/staff/urls.py
+++ CoffeeShopWebsite/staff/urls.py
@@ -1,6 +1,3 @@
 from django.urls import path
 
-urlpatterns = [
-    path("login/", CustomUserLoginView.as_view(), name="login"),
-    path("verify/", CustomUserLoginVerifyView.as_view(), name="verify"),
-]
+urlpatterns = []
