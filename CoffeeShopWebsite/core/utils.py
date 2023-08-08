6c8675d872f0a10feadfb233830d5947c775b579
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-08 03:59:02 +0330

    refactor: comment useless func

--- CoffeeShopWebsite/core/utils.py
+++ CoffeeShopWebsite/core/utils.py
@@ -2,42 +2,6 @@ from django.shortcuts import render
 from orders.models import Order
 
 
-def add_to_cart(request, response, item_pk: int) -> None:
-    """
-    sets a cookie to add items to shopping cart.
-
-    item_pk and quantity turn into a list of dictionaries which is the
-    value of the cart cookie.
-    value = [{menu_item:quantity}, ...]
-    """
-    cart = request.COOKIES.get("cart", None)
-    if not cart:
-        value = list()
-        value.append({f"{item_pk}": 1})
-        str_value = f"{value}"
-        max_age = 604800  # 7 * 24 * 60 * 60 (7 days)
-        response.set_cookie("cart", str_value, max_age=max_age)
-    else:
-        cart = eval(cart)
-        old_val = 0
-        # print(cart, "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
-        for i in range(len(cart)):
-            print(i, "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
-            for key, val in cart[i].items():
-                # print(key, val, cart[i], "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
-                if key == item_pk:
-                    old_val = val
-                    cart.pop(i)
-            if old_val == val:
-                break
-        item_dict = {item_pk: old_val + 1}
-        cart.append(item_dict)
-        str_value = f"{cart}"
-        max_age = 604800  # 7 * 24 * 60 * 60 (7 days)
-        response.set_cookie("cart", str_value, max_age=max_age)
-    return response
-
-
 def delete_cart(request, response) -> None:
     if request.COOKIES.get("cart"):
         response.delete_cookie("cart")
@@ -69,6 +33,40 @@ def access_session(request):
         return render(request, "", context)
 
 
+# def add_to_cart(request, response, item_pk: int) -> None:
+#     """
+#     sets a cookie to add items to shopping cart.
+#
+#     item_pk and quantity turn into a list of dictionaries which is the
+#     value of the cart cookie.
+#     value = [{menu_item:quantity}, ...]
+#     """
+#     cart = request.COOKIES.get("cart", None)
+#     if not cart:
+#         value = list()
+#         value.append({f"{item_pk}": 1})
+#         str_value = f"{value}"
+#         max_age = 604800  # 7 * 24 * 60 * 60 (7 days)
+#         response.set_cookie("cart", str_value, max_age=max_age)
+#     else:
+#         cart = eval(cart)
+#         old_val = 0
+#         # print(cart, "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
+#         for i in range(len(cart)):
+#             print(i, "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
+#             for key, val in cart[i].items():
+#                 # print(key, val, cart[i], "_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_")
+#                 if key == item_pk:
+#                     old_val = val
+#                     cart.pop(i)
+#             if old_val == val:
+#                 break
+#         item_dict = {item_pk: old_val + 1}
+#         cart.append(item_dict)
+#         str_value = f"{cart}"
+#         max_age = 604800  # 7 * 24 * 60 * 60 (7 days)
+#         response.set_cookie("cart", str_value, max_age=max_age)
+#     return response
 
 
 # def remove_from_cart(request, response, item_pk: int):
