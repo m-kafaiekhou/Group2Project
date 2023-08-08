4697aac1ebaba9a59c1a8579873cd324de086164
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 09:22:05 +0330

    fix: add missing / to verify url

--- CoffeeShopWebsite/staff/urls.py
+++ CoffeeShopWebsite/staff/urls.py
@@ -3,5 +3,5 @@ from .views import CustomUserLoginView, CustomUserLoginVerifyView
 
 urlpatterns = [
     path("login/", CustomUserLoginView.as_view(), name="login"),
-    path("verify", CustomUserLoginVerifyView.as_view(), name="verify"),
+    path("verify/", CustomUserLoginVerifyView.as_view(), name="verify"),
 ]
