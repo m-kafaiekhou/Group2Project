d2e27904bd17b809e4824eb47276b9e28ffea034
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 13:09:56 +0330

    chore: define a url for entering phone number

--- CoffeeShopWebsite/core/urls.py
+++ CoffeeShopWebsite/core/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-	path('', include('pages.urls'))
+    path("", include("pages.urls")),
+    path("phone/", views.PhoneNumberEntryView.as_view(), "phone_entry"),
 ]
 
-handler404 = 'pages.views.error_404_view'
+handler404 = "pages.views.error_404_view"
