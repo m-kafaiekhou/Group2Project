e317db6ba8633fce1fde7663fdf1a022c2a59a2d
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 12:27:01 +0330

    refactor: move the forms related on otp to core app

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -1,6 +1,5 @@
 # from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
-from django import forms
-from django.core.validators import RegexValidator
+
 
 #
 #
@@ -9,14 +8,3 @@ from django.core.validators import RegexValidator
 #
 #     class Meta:
 #         fields = ['phone_number', 'password']
-
-
-class CustomUserLoginForm(forms.Form):
-    _REGEX = r"09(\d{9})$"
-    phone_validator = RegexValidator(_REGEX, "The phone number provided is invalid")
-
-    phone_number = forms.CharField(max_length=14, validators=[phone_validator])
-
-
-class VerifyCodeForm(forms.Form):
-    verify_code = forms.IntegerField()
