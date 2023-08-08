c50ff5558e775eb0de6d8cee92db8e7e3fb02b55
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-08 04:07:19 +0330

    chore: delete cart view url

--- CoffeeShopWebsite/orders/urls.py
+++ CoffeeShopWebsite/orders/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("cart/", views.CartView.as_view(), name='cart'),
     path("checkout/", views.CheckoutView.as_view(), name='checkout'),
-    path("delete-cart/", views.delete_cart_view, name='delete_cart'),
+    path("delete-cart/", views.DeleteCartView.as_view(), name='delete_cart'),
 ]
