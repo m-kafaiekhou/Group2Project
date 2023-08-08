ac6a0fc1cd27f019a8304cc8a417a3d54e3ddd84
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 14:32:31 +0330

    fix: comment two lines and add 'name=' to urls name

--- CoffeeShopWebsite/core/urls.py
+++ CoffeeShopWebsite/core/urls.py
@@ -3,9 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", include("pages.urls")),
-    path("phone/", views.PhoneNumberEntryView.as_view(), "phone_entry"),
-    path("code/", views.VerificationCodeEntryView.as_view(), "code_entry"),
+    # path("", include("pages.urls")),
+    path("phone/", views.PhoneNumberEntryView.as_view(), name="phone_entry"),
+    path("code/", views.VerificationCodeEntryView.as_view(), name="code_entry"),
 ]
 
-handler404 = "pages.views.error_404_view"
+# handler404 = "pages.views.error_404_view"
