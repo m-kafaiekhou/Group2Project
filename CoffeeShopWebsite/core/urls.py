f06d0ec6681c7be2ec8e45198bd71db9f3673bce
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-08 13:31:45 +0330

    chore: define a url for entering verification code

--- CoffeeShopWebsite/core/urls.py
+++ CoffeeShopWebsite/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", include("pages.urls")),
     path("phone/", views.PhoneNumberEntryView.as_view(), "phone_entry"),
+    path("code/", views.VerificationCodeEntryView.as_view(), "code_entry"),
 ]
 
 handler404 = "pages.views.error_404_view"
