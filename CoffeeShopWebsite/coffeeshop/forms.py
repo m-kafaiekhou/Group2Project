2e03c2fb5419e55f090cd88e39972401f02d18ed
Arad <aradarvinxo@gmail.com>
2023-08-09 19:21:18 +0330

    feat: dynamic image form created + imports

--- CoffeeShopWebsite/coffeeshop/forms.py
+++ CoffeeShopWebsite/coffeeshop/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from django.forms import ModelForm
+from .models import DynamicImages
 
 class SearchMenu(forms.Form):
     search = forms.CharField()
+
+
+class DynamicImageForm(ModelForm):
+    class Meta:
+        model = DynamicImages
+        fields = "__all__"
+
