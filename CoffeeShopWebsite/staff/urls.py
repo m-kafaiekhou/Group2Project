1871dc23d01c3ac315fbb796a806629f4d397808
arta16h <sevda.hayati2015@gmail.com>
2023-08-10 00:37:45 +0330

    Chore: order-details url

--- CoffeeShopWebsite/staff/urls.py
+++ CoffeeShopWebsite/staff/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('add-category/', AddCategoryView.as_view(), name="add_category"),
     path('add-item/', AddItemView.as_view(), name="add_item"),
+    path('order-details/', OrderDetailView.as_view(), name="order_details"),
 ]
