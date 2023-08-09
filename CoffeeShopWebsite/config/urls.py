6190457db681afce35e94eb19737b2b52aa7a537
Arad <aradarvinxo@gmail.com>
2023-08-09 18:44:48 +0330

    refactor: dashboard urls included

--- CoffeeShopWebsite/config/urls.py
+++ CoffeeShopWebsite/config/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("", include("orders.urls")),
     path("", include("menus.urls")),
     path("", include("core.urls")),
+    path("", include("dashboard.urls")),
     path("staff/", include("django.contrib.auth.urls")),
 ]
 
