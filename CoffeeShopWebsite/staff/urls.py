151a267acb935599f0ef4e6421029c3f0adaf560
arta16h <sevda.hayati2015@gmail.com>
2023-08-10 00:35:14 +0330

    Chore: additem url

--- CoffeeShopWebsite/staff/urls.py
+++ CoffeeShopWebsite/staff/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('add-category/', AddCategoryView.as_view(), name="add_category")
+    path('add-category/', AddCategoryView.as_view(), name="add_category"),
+    path('add-item/', AddItemView.as_view(), name="add_item"),
 ]
