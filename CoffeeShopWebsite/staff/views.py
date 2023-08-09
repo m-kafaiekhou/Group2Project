81f232317c178224ced24c618bf8c741a4e3de7c
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-09 09:11:51 +0330

    chore: post method done

--- CoffeeShopWebsite/staff/views.py
+++ CoffeeShopWebsite/staff/views.py
@@ -122,7 +122,13 @@ class OrderDetailView(View):
         return render(request, self.template_name, context={'item': item, 'form': form})
 
     def post(self, request, *args, **kwargs):
-        pass
+        item = get_object_or_404(self.model_class, pk=kwargs['pk'])
+        form = self.form_class(request.POST, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+        redirect('order_list')
 
 
 class OrderListView(View):
