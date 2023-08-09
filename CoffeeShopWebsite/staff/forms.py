ece80dc6f9d16ed1e4bf1f65f485ccbbde895799
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-09 06:16:11 +0330

    chore: init item add form

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -1,10 +1,6 @@
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django import forms
 
 
-#
-#
-# class CustomAuthenticationForm(AuthenticationForm):
-#     phone_number = forms.CharField(widget=forms.TextInput)
-#
-#     class Meta:
-#         fields = ['phone_number', 'password']
+class AddItemForm(forms.ModelForm):
+    pass
+
