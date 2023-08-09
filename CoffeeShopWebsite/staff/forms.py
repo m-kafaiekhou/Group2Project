0469138d7afe4508061133a818b3f13354cfd373
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-09 06:44:55 +0330

    chore: add category form

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from menus.models import CafeItem
+from menus.models import CafeItem, Category
 
 
 class AddItemForm(forms.ModelForm):
     class Meta:
         model = CafeItem
+        fields = "__all__"
+
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
         fields = "__all__"
