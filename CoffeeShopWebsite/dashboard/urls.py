0465562b96429dba70ce7da001e374191d891412
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-10 06:01:50 +0330

    chore: panel

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
+from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('add-category/', AddCategoryView.as_view(), name="add_category"),
+    path('add-item/', AddItemView.as_view(), name="add_item"),
+    path('order-details/', OrderDetailView.as_view(), name="order_details"),
+    path('category-details/', CategoryDetailView.as_view(), name="category_details"),
+    path('item-details/', ItemDetailView.as_view(), name="item_details"),
+    path('category-list/', CategoryListView.as_view(), name="category_list"),
+    path('item-list/', ItemListView.as_view(), name="item_list"),
+]
