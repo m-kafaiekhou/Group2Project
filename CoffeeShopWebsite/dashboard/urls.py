37745e92a14b3ec4cb2f55edfb9563fcde395c41
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-10 07:20:18 +0330

    fix: detail urls

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('add-category/', AddCategoryView.as_view(), name="add_category"),
     path('add-item/', AddItemView.as_view(), name="add_item"),
-    path('order-details/', OrderDetailView.as_view(), name="order_details"),
-    path('category-details/', CategoryDetailView.as_view(), name="category_details"),
-    path('item-details/', ItemDetailView.as_view(), name="item_details"),
+    path('order-details/<int:pk>/', OrderDetailView.as_view(), name="order_details"),
+    path('category-details/<int:pk>/', CategoryDetailView.as_view(), name="category_details"),
+    path('item-details/<int:pk>/', ItemDetailView.as_view(), name="item_details"),
     path('category-list/', CategoryListView.as_view(), name="category_list"),
     path('item-list/', ItemListView.as_view(), name="item_list"),
     path('order-list/', OrderListView.as_view(), name="order_list"),
