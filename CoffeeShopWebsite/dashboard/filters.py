0f092b00cd5af86222ea4ca62a90bd0f902191b7
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-11 23:18:53 +0330

    chore: filter add placeholder

--- CoffeeShopWebsite/dashboard/filters.py
+++ CoffeeShopWebsite/dashboard/filters.py
@@ -1,8 +1,15 @@
 from menus.models import CafeItem
 import django_filters
+from django import forms
 
 
 class ItemFilterSet(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'placeholder': 'Enter name'})
+    )
+
     class Meta:
         model = CafeItem
         fields = ['name', 'category', 'is_available']
