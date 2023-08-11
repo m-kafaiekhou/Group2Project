519ad3d650451ad0f56beacd1dce3abddcbcd5c5
arta16h <sevda.hayati2015@gmail.com>
2023-08-11 22:04:29 +0330

    autocomplete url

--- CoffeeShopWebsite/menus/urls.py
+++ CoffeeShopWebsite/menus/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path(" ", views.autocomplete, name="autocomplete"),
     path("menu/", views.Menu.as_view(), name="menu"),
     path("menu/search/", views.MenuSearch.as_view(), name="menu_search"),
-    # path(
-    #     "menu/detail/<str:cafeitme_name>",
-    #     views.MenuDetail.as_view(),
-    #     name="menu_detail",
-    # ),
+    # path("menu/detail/<str:cafeitme_name>", views.MenuDetail.as_view(), name="menu_detail"),
 ]
