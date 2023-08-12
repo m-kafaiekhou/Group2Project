05778239341596252d7fc9e94839901c1f418022
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-13 02:40:10 +0330

    chore: changed the found function as needed

--- CoffeeShopWebsite/statics/js/order-ajax.js
+++ CoffeeShopWebsite/statics/js/order-ajax.js
@@ -1,20 +1,21 @@
-//
-// $('.delete-button').on('click', function() {
-//   var itemId = $(this).data('item-id');
-//
-//   $.ajax({
-//     url: '/delete-item',
-//     type: 'POST',
-//     data: { itemId: itemId },
-//     success: function(response) {
-//
-//       console.log('Item deleted successfully');
-//
-//       $(this).closest('.item').remove();
-//     },
-//     error: function(xhr, status, error) {
-//
-//       console.error('Error deleting item:', error);
-//     }
-//   });
-// });
+
+$('.status-button').on('click', function() {
+  let itemId = $(this).data('item-id');
+  let status = $(this).data('status');
+
+  $.ajax({
+    url: `/order-list/${status}/`,
+    type: 'POST',
+    data: { itemId: itemId },
+    success: function(response) {
+
+      console.log('Item deleted successfully');
+
+      window.location.reload();
+    },
+    error: function(xhr, status, error) {
+
+      console.error('Error deleting item:', error);
+    }
+  });
+});
