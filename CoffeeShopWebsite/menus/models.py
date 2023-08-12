f77f0fedd39c478333d6a4c7d5ffc39fc5f7a207
arta16h <sevda.hayati2015@gmail.com>
2023-08-12 20:41:48 +0330

    Reafctor: slug save function

--- CoffeeShopWebsite/menus/models.py
+++ CoffeeShopWebsite/menus/models.py
@@ -38,9 +38,9 @@ class CafeItem(models.Model):
     def get_absolute_url(self) :
         return reverse("cafeitem_detail", kwargs={"slug": self.slug})
     
-    def save(self, *args, **kwargs):  # new
+    def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
 
     @property
