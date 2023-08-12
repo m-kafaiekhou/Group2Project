921a9e964a440be756fb22a651268c493ec128f3
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-13 02:51:25 +0330

    refactor: set csrf token in header

--- CoffeeShopWebsite/statics/js/order-ajax.js
+++ CoffeeShopWebsite/statics/js/order-ajax.js
@@ -3,6 +3,14 @@ $('.status-button').on('click', function() {
   let itemId = $(this).data('item-id');
   let status = $(this).data('status');
 
+  const csrftoken = getCookie('csrftoken')
+
+  $.ajaxSetup({
+    header: {
+      'X-CSRFToken': csrftoken
+    }
+  });
+
   $.ajax({
     url: `/dashboard/order-list/${status}/`,
     type: 'POST',
@@ -19,3 +27,19 @@ $('.status-button').on('click', function() {
     }
   });
 });
+
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
