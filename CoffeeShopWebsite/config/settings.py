0a26d0a74e45d1f380ad7144687469b1b7c695fd
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-12 14:19:30 +0330

    chore: read environment variables from a .env file

--- CoffeeShopWebsite/config/settings.py
+++ CoffeeShopWebsite/config/settings.py
@@ -15,6 +15,8 @@ import environ
 
 # Initialise environment variables
 env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
