51ac368f031308caa99297b8fd856873d7a455e1
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 03:08:34 +0330

    Chore: core config

--- CoffeeShopWebsite/core/apps.py
+++ CoffeeShopWebsite/core/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+class MainAdminConfig(AdminConfig) :
+    default_site = 'core.admin.MainAdminArea'
