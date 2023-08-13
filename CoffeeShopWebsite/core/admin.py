5329c87a17eb28e4e89661457ea28c2e23e6920e
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 01:32:54 +0330

    Chore: models imported

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from menus.models import CafeItem, Category
+from orders.models import Order, OrderItem
 # Register your models here.
 class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
