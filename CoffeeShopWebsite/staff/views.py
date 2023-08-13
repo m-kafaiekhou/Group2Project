77f948538ecb826a7ed4fd9e5080512f8a57fea4
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-13 22:29:26 +0330

    refactor: delete print statments

--- CoffeeShopWebsite/staff/views.py
+++ CoffeeShopWebsite/staff/views.py
@@ -12,26 +12,25 @@ class LoginUserView(View):
 
     def get(self, request, *args, **kwargs):
         form = CustomAuthenticationForm()
-        return render(request, self.template_name, context={'form': form})
+        return render(request, self.template_name, context={"form": form})
 
     def post(self, request, *args, **kwargs):
         print(request.POST)
         form = CustomAuthenticationForm(request.POST)
         try:
             if form.is_valid():
-                print("**************************************")
-                print(form.cleaned_data)
                 CUB = CustomUserBackend()
-                user = CUB.authenticate(request, phone_number=form.cleaned_data["phone_number"],
-                                                      password=form.cleaned_data["password"])
-                print("********after")
+                user = CUB.authenticate(
+                    request,
+                    phone_number=form.cleaned_data["phone_number"],
+                    password=form.cleaned_data["password"],
+                )
                 if user:
-                    print("**************************************")
                     set_otp(request, user.phone_number)
                     return redirect("code_entry")
             else:
                 messages.error(request, "Wrong Credentials!!")
-                return render(request, self.template_name, context={'form': form})
+                return render(request, self.template_name, context={"form": form})
         except:
             messages.error(request, "Please enter email and password for login!")
-            return redirect('login')
+            return redirect("login")
