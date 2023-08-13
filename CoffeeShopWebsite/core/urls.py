e666a67a508ab0d6f801de6efa759b2612bbe2bc
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 00:36:52 +0330

    Chore: admin panel site title

--- CoffeeShopWebsite/core/urls.py
+++ CoffeeShopWebsite/core/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 
 # handler404 = "pages.views.error_404_view"
 admin.site.index_title = 'Group2 CoffeeShop'
-admin.site.site_header_header = 'CoffeeShop Admin'
+admin.site.site_header = 'CoffeeShop Admin'
+admin.site.site_title = 'CoffeeShop Admin'
