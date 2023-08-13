bf31d9432831ab606b6ef51bfa6199c86f5ccd7c
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 01:43:24 +0330

    Refactor: imports

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from menus.models import CafeItem, Category
-from orders.models import Order, OrderItem
 # Register your models here.
 class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
