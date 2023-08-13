c1deadccb38b451c7b60e21f68728d3d8684531f
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 01:44:44 +0330

    Refactor: register refactored

--- CoffeeShopWebsite/orders/admin.py
+++ CoffeeShopWebsite/orders/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from core.admin import main_site
 
 # Register your models here.
 
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+main_site.register(Order)
+main_site.register(OrderItem)
