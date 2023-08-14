74ad97b575da93f7c7cbb3f5709f7c8386c96edd
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 10:00:42 +0330

    refactor: change password field to registration code

--- CoffeeShopWebsite/staff/forms.py
+++ CoffeeShopWebsite/staff/forms.py
@@ -4,7 +4,7 @@ from .models import CustomUserModel
 
 class CustomAuthenticationForm(forms.Form):
     phone_number = forms.CharField(max_length=14)
-    password = forms.CharField(max_length=128, widget=forms.PasswordInput())
+    registration_code = forms.CharField(max_length=4, widget=forms.PasswordInput())
     # class Meta:
     #     model = CustomUserModel
     #     fields = ('phone_number', 'password', )
