eb0ecf2e0e78d60da623a64690cca4007d7a93e2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 15:04:45 +0330

    style: change the place of commented codes

--- CoffeeShopWebsite/staff/backends.py
+++ CoffeeShopWebsite/staff/backends.py
@@ -9,23 +9,6 @@ import datetime
 
 
 class CustomUserBackend(ModelBackend):
-    # def authenticate(self, request, phone_number=None, password=None, **kwargs):
-    #     username = phone_number
-
-    #     if username is None:
-    #         username = kwargs.get(self.UserModel.USERNAME_FIELD)
-    #     if username is None or password is None:
-    #         return
-    #     try:
-    #         user = self.UserModel._default_manager.get_by_natural_key(username)
-    #     except self.UserModel.DoesNotExist:
-    #         # Run the default password hasher once to reduce the timing
-    #         # difference between an existing and a nonexistent user (#20760).
-    #         self.UserModel().set_password(password)
-    #     else:
-    #         if user.check_password(password) and self.user_can_authenticate(user):
-    #             return user
-
     def authenticate(self, request, phone_number=None, otp_code=None, **kwargs):
         username = phone_number
 
@@ -56,3 +39,21 @@ class CustomUserBackend(ModelBackend):
         except CustomUserModel.DoesNotExist:
             del request.session["otp"]
             return None
+
+    # def authenticate(self, request, phone_number=None, password=None, **kwargs):
+
+    #     username = phone_number
+
+    #     if username is None:
+    #         username = kwargs.get(self.UserModel.USERNAME_FIELD)
+    #     if username is None or password is None:
+    #         return
+    #     try:
+    #         user = self.UserModel._default_manager.get_by_natural_key(username)
+    #     except self.UserModel.DoesNotExist:
+    #         # Run the default password hasher once to reduce the timing
+    #         # difference between an existing and a nonexistent user (#20760).
+    #         self.UserModel().set_password(password)
+    #     else:
+    #         if user.check_password(password) and self.user_can_authenticate(user):
+    #             return user
