554f0c7dc6908a354faff6f8541625eac4a4dc01
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 08:59:54 +0330

    refactor: edit post method in VerificationCodeEntryView

--- CoffeeShopWebsite/core/views.py
+++ CoffeeShopWebsite/core/views.py
@@ -20,16 +20,19 @@ class VerificationCodeEntryView(View):
 
     def post(self, request):
         form = self.form_class(request.POST)
+        otp_session = request.session.get("otp")
         if form.is_valid():
             input_code = form.cleaned_data["verification_code"]
-            sent_code = request.session.get("otp_code")
+            sent_code = otp_session.get("code")
             if sent_code and input_code == sent_code:
-                user = get_object_or_404(CustomUserModel, phone_number=request.session['phone_number'])
-                del request.session["otp_code"]
+                user = get_object_or_404(
+                    CustomUserModel, phone_number=request.session["phone_number"]
+                )
+                del request.session["otp"]
                 login(request, user)
                 return redirect("home")
             else:
-                del request.session["otp_code"]
-                messages.error(request, "The otp code is wrong!", "danger")
+                del request.session["otp"]
+                messages.error(request, "The otp code is wrong. Try again!", "danger")
                 return redirect("phone_entry")
         return render(request, "core/code_entry.html", {"form": form})
