4f2a074f907b3ac6aee66761983d4cd23a528fb0
Arad <aradarvinxo@gmail.com>
2023-08-14 13:33:53 +0330

    chore: month dict function added

--- CoffeeShopWebsite/dashboard/chart_utils.py
+++ CoffeeShopWebsite/dashboard/chart_utils.py
@@ -5,6 +5,9 @@ months = [
     "September", "October", "November", "December"
 ]
 
+range_m = list(range(1,31+1))
+month = list(map(str,range_m))
+
 def year_dict():
     year_dict = dict()
 
@@ -12,3 +15,12 @@ def year_dict():
         year_dict[month] = 0
     
     return year_dict
+
+
+def month_dict():
+    month_dict = dict()
+
+    for day in month:
+        month_dict[day] = 0
+    
+    return month_dict
