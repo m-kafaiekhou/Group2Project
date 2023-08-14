3f8c2e45ee3f92884a06172fae94c9fa3d360443
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 03:55:08 +0330

    Refactor: dashboard view

--- CoffeeShopWebsite/dashboard/views.py
+++ CoffeeShopWebsite/dashboard/views.py
@@ -232,4 +232,7 @@ class OrderListView(LoginRequiredMixin, View):
             return JsonResponse({'error': 'Order not found'}, status=404)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
+        
+class DashboardView(View) :
+    pass
 
