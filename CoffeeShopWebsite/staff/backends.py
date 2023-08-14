a88a8566af819b206cfb7d8430447e80f70640ba
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 12:31:30 +0330

    chore: import get_object_or_404

--- CoffeeShopWebsite/staff/backends.py
+++ CoffeeShopWebsite/staff/backends.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.http.request import HttpRequest
+from django.shortcuts import get_object_or_404
 from .models import CustomUserModel
 import datetime
 
