7094927f27c7432a0d8b7f01772728ea8677e01c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 20:36:55 +0330

    refactor: edit urls

--- CoffeeShopWebsite/menus/urls.py
+++ CoffeeShopWebsite/menus/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.autocomplete.as_view(), name="autocomplete"),
-    path("menu/", views.Menu.as_view(), name="menu"),
-    path("menu/search/", views.MenuSearch.as_view(), name="menu_search"),
+    path("auto/", views.autocomplete.as_view(), name="autocomplete"),
+    path("", views.Menu.as_view(), name="menu"),
+    path("search/", views.MenuSearch.as_view(), name="menu_search"),
     # path("menu/detail/<str:cafeitme_name>", views.MenuDetail.as_view(), name="menu_detail"),
 ]
