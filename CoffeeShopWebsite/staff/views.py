b215c6440562941bdf5697a0b13aaf935d9eb59f
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 11:23:45 +0330

    refactor: comment post method and import PhoneNumberForm and OtpForm

--- CoffeeShopWebsite/staff/views.py
+++ CoffeeShopWebsite/staff/views.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .forms import CustomAuthenticationForm
+from .forms import PhoneNumberForm, OtpForm
 from staff.backends import CustomUserBackend
 from core.utils import set_otp
 
@@ -14,23 +14,23 @@ class LoginUserView(View):
         form = CustomAuthenticationForm()
         return render(request, self.template_name, context={"form": form})
 
-    def post(self, request, *args, **kwargs):
-        print(request.POST)
-        form = CustomAuthenticationForm(request.POST)
-        try:
-            if form.is_valid():
-                CUB = CustomUserBackend()
-                user = CUB.authenticate(
-                    request,
-                    phone_number=form.cleaned_data["phone_number"],
-                    password=form.cleaned_data["password"],
-                )
-                if user:
-                    set_otp(request, user.phone_number)
-                    return redirect("code_entry")
-            else:
-                messages.error(request, "Wrong Credentials!!")
-                return render(request, self.template_name, context={"form": form})
-        except:
-            messages.error(request, "Please enter email and password for login!")
-            return redirect("login")
+    # def post(self, request, *args, **kwargs):
+    #     print(request.POST)
+    #     form = CustomAuthenticationForm(request.POST)
+    #     try:
+    #         if form.is_valid():
+    #             CUB = CustomUserBackend()
+    #             user = CUB.authenticate(
+    #                 request,
+    #                 phone_number=form.cleaned_data["phone_number"],
+    #                 password=form.cleaned_data["password"],
+    #             )
+    #             if user:
+    #                 set_otp(request, user.phone_number)
+    #                 return redirect("code_entry")
+    #         else:
+    #             messages.error(request, "Wrong Credentials!!")
+    #             return render(request, self.template_name, context={"form": form})
+    #     except:
+    #         messages.error(request, "Please enter email and password for login!")
+    #         return redirect("login")
