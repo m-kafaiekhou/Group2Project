7df8663a1a2f67799546656e2be968c518b79a07
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 03:57:28 +0330

    Chore: importing models

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
+import django.apps
 # Register your models here.
+models = django.apps.apps.get_models()
 class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
 
 main_site = MainAdminArea(name='MainAdmin')
+
+
