e2bcad413b7c42e136547e3c74ab8b85e66e5fa3
Arad <aradarvinxo@gmail.com>
2023-08-14 11:59:33 +0330

    chore: wrote a function to return year dict

--- CoffeeShopWebsite/dashboard/chart_utils.py
+++ CoffeeShopWebsite/dashboard/chart_utils.py
@@ -4,3 +4,11 @@ months = [
     "May", "June", "July", "August",
     "September", "October", "November", "December"
 ]
+
+def year_dict():
+    year_dict = dict()
+
+    for month in months:
+        year_dict[month] = 0
+    
+    return year_dict
