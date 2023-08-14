5360f5e0aacb2cef99a824712a70febb776506a8
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 04:05:57 +0330

    Chore: models for loop

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 import django.apps
 # Register your models here.
+
 models = django.apps.apps.get_models()
+
+for model in models :
+    try :
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered :
+        pass
+
 class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
+
 
 main_site = MainAdminArea(name='MainAdmin')
 
