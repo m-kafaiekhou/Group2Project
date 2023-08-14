584e05e2bb0465d463b511f54e9d9cc7dd9afc60
arta16h <sevda.hayati2015@gmail.com>
2023-08-14 18:15:47 +0330

    Chore: static root

--- CoffeeShopWebsite/config/settings.py
+++ CoffeeShopWebsite/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 import environ
+import os
 
 # Initialise environment variables
 env = environ.Env()
@@ -139,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [BASE_DIR / "statics"]
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
