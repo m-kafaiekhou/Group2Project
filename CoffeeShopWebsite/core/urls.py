7b2f03b40fc4cc712adebc14a0176bf89d6c57ef
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-14 13:05:42 +0330

    refactor: comment additional codes

--- CoffeeShopWebsite/core/urls.py
+++ CoffeeShopWebsite/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # path("", include("pages.urls")),
     # path("phone/", views.PhoneNumberEntryView.as_view(), name="phone_entry"),
-    path("code/", views.VerificationCodeEntryView.as_view(), name="code_entry"),
+    # path("code/", views.VerificationCodeEntryView.as_view(), name="code_entry"),
 ]
 
 # handler404 = "pages.views.error_404_view"
