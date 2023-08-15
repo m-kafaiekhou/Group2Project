ad324fd916923f31e2851e301c568f2a35b5cec3
arta16h <sevda.hayati2015@gmail.com>
2023-08-16 01:51:40 +0330

    Refactor: category-cafeitem registered

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -3,12 +3,9 @@ import django.apps
 # Register your models here.
 
 
-# admin.site.unregister(django.contrib.sessions.models.Session)
-
 class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
     login_template = 'core/admin/login.html'
 
 
 main_site = MainAdminArea(name='MainAdmin')
-
--- CoffeeShopWebsite/menus/admin.py
+++ CoffeeShopWebsite/menus/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import CafeItem, Category
-from core.admin import main_site
 # Register your models here.
 
 
-main_site.register(Category)
-main_site.register(CafeItem)
+admin.site.register(Category)
+admin.site.register(CafeItem)
