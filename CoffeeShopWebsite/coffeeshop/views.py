fcd864a4acef6483cb580bd642e1d2e8bb0f337c
mohammadali127 <m.a.soltanhosseini@gmail.com>
2023-08-15 11:22:13 +0330

    chore: generic view for coffeeshop/views.py

--- CoffeeShopWebsite/coffeeshop/views.py
+++ CoffeeShopWebsite/coffeeshop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from menus.models import CafeItem
+from django.views.generic import ListView,TemplateView
 # Create your views here.
 
 
@@ -11,6 +12,13 @@ class HomePage(View):
         # top_rated_items = CafeItem.top_rated_items()
         return render(request, self.template_name, {"top_rated_items": None})
 
+'''class HomePage(TemplateView):
+    template_name = "coffeeshop/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['top_rated_items'] = None
+        return context'''
+
 
 class GalleryPage(View):
     template_name = "coffeeshop/gallery.html"
@@ -19,3 +27,9 @@ class GalleryPage(View):
         items = CafeItem.objects.all()
         context = {"items":items}
         return render(request, self.template_name, context)
+
+'''class GalleryPage(ListView):
+    template_name = "coffeeshop/gallery.html"
+    model = CafeItem
+    context_object_name = "items "
+'''
