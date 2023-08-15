1a3643c0a95753a3b4751f7ecc36db41434afcb4
Arad <aradarvinxo@gmail.com>
2023-08-15 20:41:42 +0330

    chore: url for daily sum added

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -24,6 +24,7 @@ urlpatterns += [
     path("chart/sales/this-year/", yearly_sales_chart, name="this-year-sales"),
     path("chart/sales/this-month/", monthly_sales_chart, name="month-sales"),
     path("chart/sales/this-day/", daily_sales_chart, name="day-sales"),
+    path("chart/sales/daily-sum/", daily_sales_chart, name="day-sales-sum"),
     path("chart/sales/all-time/", all_time_sales, name="all-time-sales"),
     path("chart/sales/top-selling/", top_10_selling_items, name="top-selling"),
     path("chart/sales/best-customers/", top_10_customers, name="best-customers"),
