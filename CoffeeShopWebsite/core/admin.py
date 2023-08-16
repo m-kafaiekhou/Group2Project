44dc6ee5b4149812004b269728455ac3c429ff2b
arta16h <sevda.hayati2015@gmail.com>
2023-08-16 12:27:40 +0330

    Refactor: admin panel style changed

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 import django.apps
+
 # Register your models here.
 
 
@@ -7,5 +8,10 @@ class MainAdminArea(admin.AdminSite) :
     site_header = 'Main Admin Area'
     login_template = 'core/admin/login.html'
 
-
 main_site = MainAdminArea(name='MainAdmin')
+
+
+class FilterCafeitem(admin.ModelAdmin) :
+    list_display = ('name', 'is_available', 'price', 'category')
+    list_filter = ('is_available', 'price')
+
