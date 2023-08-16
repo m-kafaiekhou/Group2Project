dd993f3c83adb1fddfcbe811befc45967584a2d6
Arad <aradarvinxo@gmail.com>
2023-08-16 16:02:37 +0330

    chore: url for employee sales

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -29,4 +29,5 @@ urlpatterns += [
     path("chart/sales/top-selling/", top_10_selling_items, name="top-selling"),
     path("chart/sales/best-customers/", top_10_customers, name="best-customers"),
     path("chart/sales/category-sale/", sales_by_category, name="category-sale"),
+    path("chart/sales/employee-sale/", sales_by_employee, name="employee-sale"),
 ]
