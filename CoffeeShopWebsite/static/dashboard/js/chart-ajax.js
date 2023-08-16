d649c3a35913a08d3e3622c8c01ef328961c3e87
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-17 02:34:23 +0330

    chore: filter by span done

--- CoffeeShopWebsite/static/dashboard/js/chart-ajax.js
+++ CoffeeShopWebsite/static/dashboard/js/chart-ajax.js
@@ -97,11 +97,17 @@ function loadChart(chart, endpoint) {
     });
 }
 
-function loadAllCharts(chart1, chart2, chart3, chart4) {
-    loadChart(chart1, `/dashboard/chart/sales/this-year/`);
+function loadAllCharts(BigMainChart, chart2, chart3, chart4) {
+    loadChart(BigMainChart, links["day-sales"]);
     loadChart(chart2, `/dashboard/chart/sales/this-month/`);
-    loadChart(chart3, `/dashboard/chart/sales/this-year/`);
-    loadChart(chart4, `/dashboard/chart/sales/this-month/`);
+    loadChart(chart1, `/dashboard/chart/sales/this-day/`);
+    loadChart(chart4, `/dashboard/chart/sales/this-day/`);
+}
+
+links = {
+  'year-sales': `/dashboard/chart/sales/this-year/`,
+  'month-sales': `/dashboard/chart/sales/this-month/`,
+  'day-sales': `/dashboard/chart/sales/this-day/`,
 }
 
 type = ['primary', 'info', 'success', 'warning', 'danger'];
@@ -485,7 +491,7 @@ demo = {
     };
 
     var chart1 = new Chart(ctx, {
-      type: 'line',
+      type: 'bar',
       data: data,
       options: gradientChartOptionsConfigurationWithTooltipPurple
     });
@@ -541,7 +547,7 @@ demo = {
     gradientStroke.addColorStop(0.4, 'rgba(72,72,176,0.0)');
     gradientStroke.addColorStop(0, 'rgba(119,52,169,0)'); //purple colors
     var config = {
-      type: 'line',
+      type: 'bar',
       data: {
         labels: chart_labels,
         datasets: [{
@@ -564,27 +570,19 @@ demo = {
       },
       options: gradientChartOptionsConfigurationWithTooltipPurple
     };
-    let chart3 = new Chart(ctx, config);
+    let BigMainChart = new Chart(ctx, config);
     $("#0").click(function() {
-      var data = myChartData.config.data;
-      data.datasets[0].data = chart_data;
-      data.labels = chart_labels;
-      myChartData.update();
+      loadChart(BigMainChart, links["day-sales"])
+      BigMainChart.update();
     });
     $("#1").click(function() {
-      var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
-      var data = myChartData.config.data;
-      data.datasets[0].data = chart_data;
-      data.labels = chart_labels;
-      myChartData.update();
+      loadChart(BigMainChart, links["month-sales"])
+      BigMainChart.update();
     });
 
     $("#2").click(function() {
-      var chart_data = [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130];
-      var data = myChartData.config.data;
-      data.datasets[0].data = chart_data;
-      data.labels = chart_labels;
-      myChartData.update();
+      loadChart(BigMainChart, links["year-sales"])
+      BigMainChart.update();
     });
 
 
@@ -605,214 +603,18 @@ demo = {
       },
       options: gradientBarChartConfiguration
     })
-      loadAllCharts(chart1, chart2, chart3, chart4)
+      loadAllCharts(BigMainChart, chart2, chart1, chart4)
+
+    let element = document.getElementById('#month-big')
+
+    function activeMonthChart(event){
+      loadChart(BigMainChart, links["month-sales"])
+    }
+
+    element.addEventListener('click', activeMonthChart)
   },
 
-  initGoogleMaps: function() {
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-      zoom: 13,
-      center: myLatlng,
-      scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-      styles: [{
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#8ec3b9"
-          }]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1a3646"
-          }]
-        },
-        {
-          "featureType": "administrative.country",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#4b6878"
-          }]
-        },
-        {
-          "featureType": "administrative.land_parcel",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#64779e"
-          }]
-        },
-        {
-          "featureType": "administrative.province",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#4b6878"
-          }]
-        },
-        {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#334e87"
-          }]
-        },
-        {
-          "featureType": "landscape.natural",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#283d6a"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#6f9ba5"
-          }]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#3C7680"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#304a7d"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#98a5be"
-          }]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#2c6675"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#9d2a80"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [{
-            "color": "#9d2a80"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#b0d5ce"
-          }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#023e58"
-          }]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#98a5be"
-          }]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "labels.text.stroke",
-          "stylers": [{
-            "color": "#1d2c4d"
-          }]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry.fill",
-          "stylers": [{
-            "color": "#283d6a"
-          }]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#3a4762"
-          }]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [{
-            "color": "#0e1626"
-          }]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-            "color": "#4e6d70"
-          }]
-        }
-      ]
-    };
 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title: "Hello World!"
-    });
-
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  },
 
   showNotification: function(from, align) {
     color = Math.floor((Math.random() * 4) + 1);
