306ffaadd60059cb6d7cce10ea77b3057c9234ee
Arad <aradarvinxo@gmail.com>
2023-08-16 18:49:18 +0330

    chore: url for most popular items(top 3)

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -29,6 +29,6 @@ urlpatterns += [
     path("chart/sales/top-selling/", top_10_selling_items, name="top-selling"),
     path("chart/sales/best-customers/", top_10_customers, name="best-customers"),
     path("chart/sales/category-sale/", sales_by_category, name="category-sale"),
-    path("chart/sales/employee-sale/", sales_by_employee, name="employee-sale"),
     path("chart/sales/peak-hour/", peak_business_hour, name="peak-hour"),
+    path("chart/sales/popular-items/", most_popular_items, name="popular-items"),
 ]
