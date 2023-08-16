30aa96ec0bf0620f05d442587beec66212bf514b
Arad <aradarvinxo@gmail.com>
2023-08-16 20:52:12 +0330

    chore: url for order status report

--- CoffeeShopWebsite/dashboard/urls.py
+++ CoffeeShopWebsite/dashboard/urls.py
@@ -32,4 +32,5 @@ urlpatterns += [
     path("chart/sales/employee-sales/", sales_by_employee, name="employee-sales"),
     path("chart/sales/peak-hour/", peak_business_hour, name="peak-hour"),
     path("chart/sales/popular-items/", most_popular_items, name="popular-items"),
+    path("chart/sales/status/<int:day><str:status>/", order_status_report, name="status"),
 ]
