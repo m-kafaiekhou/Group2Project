c7b27a8e8ec5851cae5e6f82c86ac2720f94bd15
arta16h <sevda.hayati2015@gmail.com>
2023-08-17 03:46:20 +0330

    Chore: actions

--- CoffeeShopWebsite/core/admin.py
+++ CoffeeShopWebsite/core/admin.py
@@ -14,16 +14,17 @@ main_site = MainAdminArea(name='MainAdmin')
 
 
 class FilterCafeitem(admin.ModelAdmin) :
+    actions = ('make_cafeitems_unavailable', 'make_cafeitems_available')
     list_display = ('name', 'price', 'category', 'is_available')
     list_filter = ('is_available', )
     list_per_page = 15
     def make_cafeitems_unavailable(self, modeladmin,request, queryset):
         queryset.update(is_available=False)
-    make_cafeitems_unavailable.short_description = "Marks selected Cafe Items as unavailable"
+    make_cafeitems_unavailable.short_description = "Mark selected Cafe Items as unavailable"
 
     def make_cafeitems_available(self, modeladmin,request, queryset):
         queryset.update(is_available=True)
-    make_cafeitems_available.short_description = "Marks selected Cafe Items as available"
+    make_cafeitems_available.short_description = "Mark selected Cafe Items as available"
     
 
 class FilterCategory(admin.ModelAdmin) :
