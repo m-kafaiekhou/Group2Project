731e29f1e8efdb4066c4e07bab1ff7d42a7266b8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-17 03:54:17 +0330

    chore: register models

--- CoffeeShopWebsite/coffeeshop/admin.py
+++ CoffeeShopWebsite/coffeeshop/admin.py
@@ -4,3 +4,6 @@ from .models import Review, Footer, HomePage, CarouselItem, Service
 
 admin.site.register(Review)
 admin.site.register(Footer)
+admin.site.register(HomePage)
+admin.site.register(CarouselItem)
+admin.site.register(Service)
