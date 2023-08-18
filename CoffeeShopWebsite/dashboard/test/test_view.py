00737b5774e20d2b636569b2de49663f9824f393
Arad <aradarvinxo@gmail.com>
2023-08-18 15:27:07 +0330

    chore: test for add item url

--- CoffeeShopWebsite/dashboard/test/test_view.py
+++ CoffeeShopWebsite/dashboard/test/test_view.py
@@ -7,3 +7,5 @@ class DashboardTests(TestCase):
     def test_url_exits_at_correct_location_for_list_view(self):
         response1 = self.client.get("add-category/")
         self.assertNotEqual(response1.status_code, 200)
+        response2 = self.client.get("add-item/")
+        self.assertNotEqual(response2.status_code, 200)
