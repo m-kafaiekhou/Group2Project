a080d37b0a665f3a7a1ef18014d9911c71ed85eb
arta16h <sevda.hayati2015@gmail.com>
2023-08-18 22:54:57 +0330

    Chore: UrlTestClass

--- CoffeeShopWebsite/menus/test/test_urls.py
+++ CoffeeShopWebsite/menus/test/test_urls.py
@@ -1,3 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse,resolve
 from menus.views import MenuDetail, MenuSearch, Menu, autocomplete
+
+class UrlsTestClass(SimpleTestCase):
+    pass
