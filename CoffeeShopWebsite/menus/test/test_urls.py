d719f60a64fd5860c822bc3e0d78d682eee01bf7
arta16h <sevda.hayati2015@gmail.com>
2023-08-18 22:59:41 +0330

    Chore: test-menu

--- CoffeeShopWebsite/menus/test/test_urls.py
+++ CoffeeShopWebsite/menus/test/test_urls.py
@@ -3,4 +3,7 @@ from django.urls import reverse,resolve
 from menus.views import MenuDetail, MenuSearch, Menu, autocomplete
 
 class UrlsTestClass(SimpleTestCase):
-    pass
+        
+    def test_menu(self):
+        url=reverse("menus:menu")
+        self.assertEquals(resolve(url).func.view_class,Menu)
