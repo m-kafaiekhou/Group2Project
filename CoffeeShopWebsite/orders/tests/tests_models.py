6c7e5e80a39b0bc77234a3ef8255e4a71c87c5b4
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-18 15:13:45 +0330

    chore: init model test for order

---
+++ CoffeeShopWebsite/orders/tests/tests_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from orders.models import Order
+
+
+class OrderTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_status(self):
+        pass
+
+    def test_phone_number(self):
+        pass
+
+    def test_order_date(self):
+        pass
+
+    def test_table_number(self):
+        pass
+
+    def test_staff(self):
+        pass
+
+    def test_get_order_items(self):
+        pass
+
+    def test_get_total_price(self):
+        pass
