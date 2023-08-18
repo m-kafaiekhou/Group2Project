f53e6909c7a1d2336f2a25cde58c1458a086cdfd
arta16h <sevda.hayati2015@gmail.com>
2023-08-18 14:58:59 +0330

    Chore: CafeItemTestClass

--- CoffeeShopWebsite/menus/test/test_models.py
+++ CoffeeShopWebsite/menus/test/test_models.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+class CafeItemTestClass(TestCase) :
+    @classmethod
+    def setUpTestData(cls) -> None:
+        return super().setUpTestData()
+    
+    def setUp(self) -> None:
+        return super().setUp()
+    
