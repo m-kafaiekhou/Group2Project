2ea25592cec47a12e98de563738f820e3f0d063b
Arad <aradarvinxo@gmail.com>
2023-08-18 15:22:53 +0330

    chore: add response test for add category

---
+++ CoffeeShopWebsite/dashboard/test/test_view.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+class DashboardTests(TestCase):
+
+    def test_url_exits_at_correct_location_for_list_view(self):
+        response1 = self.client.get("add-category/")
+        self.assertNotEqual(response1.status_code, 200)
