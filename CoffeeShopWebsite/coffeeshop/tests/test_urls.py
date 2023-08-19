f41eb5529fab2e9f5dffdeb63a1e981817138084
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-19 22:21:56 +0330

    test: define test_home_page_url to check home page url works correctly

--- CoffeeShopWebsite/coffeeshop/tests/test_urls.py
+++ CoffeeShopWebsite/coffeeshop/tests/test_urls.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 class HomePageUrlTestCase(TestCase):
+    def test_home_page_url(self):
+        url = reverse("coffeeshop:home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "coffeeshop/home.html")
