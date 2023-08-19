8789400c2e7e55fa217daad9883f94e50f3c6f65
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-19 22:16:22 +0330

    test: define test_default_values to check default values

--- CoffeeShopWebsite/coffeeshop/tests/test_models.py
+++ CoffeeShopWebsite/coffeeshop/tests/test_models.py
@@ -52,3 +52,29 @@ class FooterModelTest(TestCase):
         self.assertEqual(Footer.objects.count(), 1)
         self.assertEqual(self.footer.footer_name, "test footer")
 
+    def test_default_vlaues(self):
+        self.assertEqual(self.footer.footer_is_active, True)
+        self.assertEqual(self.footer.contact_us_title, "GET IN TOUCH")
+        self.assertEqual(self.footer.address, "123 Street, New York, USA")
+        self.assertEqual(self.footer.phone_number, "0211234567")
+        self.assertEqual(self.footer.email, "info@example.com")
+        self.assertEqual(self.footer.contact_us_is_active, True)
+        self.assertEqual(self.footer.follow_us_title, "FOLLOW US")
+        self.assertEqual(self.footer.twitter_link, "#")
+        self.assertEqual(self.footer.facebook_link, "#")
+        self.assertEqual(self.footer.linkedin_link, "#")
+        self.assertEqual(self.footer.instagram_link, "#")
+        self.assertEqual(self.footer.follow_us_is_active, True)
+        self.assertEqual(self.footer.open_hours_title, "OPEN HOURS")
+        self.assertEqual(self.footer.days1, "Monday- Friday")
+        self.assertEqual(self.footer.hours1, "8:00 AM- 10:00 PM")
+        self.assertEqual(self.footer.days2, "Saturday- Sunday")
+        self.assertEqual(self.footer.hours2, "2:00 PM- 10:00 PM")
+        self.assertEqual(self.footer.open_hours_is_active, True)
+        self.assertEqual(self.footer.newsletter_title, "NEWSLETTER")
+        self.assertEqual(
+            self.footer.newsletter_description,
+            "Enter your email to get the latest news",
+        )
+        self.assertEqual(self.footer.newsletter_is_active, True)
+
