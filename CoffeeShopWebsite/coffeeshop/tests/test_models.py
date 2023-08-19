6dd4dcb9fd7b6f40962e02c91239c8d2d20ced53
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-19 22:08:39 +0330

    test: define setUpTestData class method and create instances from Category, CafeItem and Review models

--- CoffeeShopWebsite/coffeeshop/tests/test_models.py
+++ CoffeeShopWebsite/coffeeshop/tests/test_models.py
@@ -2,3 +2,18 @@ from django.test import TestCase
 from coffeeshop.models import Review, Footer
 from menus.models import CafeItem, Category
 class ReviewModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name="test category")
+        cls.cafeitem = CafeItem.objects.create(
+            name="test",
+            description="for test",
+            is_available=True,
+            price=10,
+            category=cls.category,
+        )
+        cls.review = Review.objects.create(
+            rating=Review.Rating.VERY_HIGH,
+            review="This is test review",
+            cafeitem=cls.cafeitem,
+        )
