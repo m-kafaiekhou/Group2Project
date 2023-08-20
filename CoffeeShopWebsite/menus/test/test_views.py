f00310f054f263dbebb06ebac6f85dc69900eab0
arta16h <sevda.hayati2015@gmail.com>
2023-08-20 10:11:10 +0330

    Refactor: setup instead of setuptestdata

--- CoffeeShopWebsite/menus/test/test_views.py
+++ CoffeeShopWebsite/menus/test/test_views.py
@@ -1,4 +1,22 @@
 from django.test import TestCase
 from django.urls import reverse
 from views import Menu, MenuDetail, MenuSearch
+from models import Category, CafeItem
 
+
+class MenuViewTest(TestCase):
+    
+    def setUp(self) -> None:
+        self.category = Category.objects.create(
+            parent_category = 'Parent Test',
+            name = 'Cat Test',
+        )
+        
+        self.cafeitem = CafeItem.objects.create(
+            name='Test', 
+            description='Just testing...',
+            is_available=True,
+            price=50,
+            category=self.category,
+        )
+        
