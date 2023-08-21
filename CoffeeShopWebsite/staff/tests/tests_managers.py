3f85bf10cba52b3b0c235296a9376af58def5ece
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-21 20:13:18 +0330

    fix: tests fix by using model and not manager

--- CoffeeShopWebsite/staff/tests/tests_managers.py
+++ CoffeeShopWebsite/staff/tests/tests_managers.py
@@ -7,14 +7,13 @@ from staff.models import CustomUserModel
 class CustomUserManagerTestCase(TestCase):
     def setUp(self):
         self.model = CustomUserModel
-        self.manager = CustomUserManager()
 
     def test_create_user(self):
         phone_number = '1234567890'
         first_name = 'John'
         last_name = 'Doe'
         password = 'Password@123'
-        user = self.manager.create_user(phone_number, first_name, last_name, password)
+        user = self.model.objects.create_user(phone_number, first_name, last_name, password)
         self.assertEqual(user.phone_number, phone_number)
         self.assertEqual(user.first_name, first_name)
         self.assertEqual(user.last_name, last_name)
@@ -28,7 +27,7 @@ class CustomUserManagerTestCase(TestCase):
         first_name = 'John'
         last_name = 'Doe'
         password = 'Password@123'
-        user = self.manager.create_superuser(phone_number, first_name, last_name, password)
+        user = self.model.objects.create_superuser(phone_number, first_name, last_name, password)
         self.assertEqual(user.phone_number, phone_number)
         self.assertEqual(user.first_name, first_name)
         self.assertEqual(user.last_name, last_name)
