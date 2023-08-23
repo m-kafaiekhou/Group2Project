bd1aaea142cde923070c0da872a385a4cf8db0fc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-23 14:11:45 +0330

    refactor: define footer_context to access footer in templates

--- CoffeeShopWebsite/coffeeshop/context_processors.py
+++ CoffeeShopWebsite/coffeeshop/context_processors.py
@@ -3,6 +3,5 @@ from .models import Footer
 
 
 def homepage_context(request):
-    return {
-        "homepage": HomePage.objects.all(),
-    }
+def footer_context(request):
+    return {"footer": Footer.objects.all()}
