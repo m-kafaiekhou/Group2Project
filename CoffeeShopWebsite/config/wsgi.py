6fe408d5b7678abbf76f9ca1fd779d6bff522a3c
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-31 02:51:31 +0330

    chore: conf for server

--- CoffeeShopWebsite/config/wsgi.py
+++ CoffeeShopWebsite/config/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
