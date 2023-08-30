51b8ac0e120a0a38a66a41e57c6441340b816b6b
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-08-31 03:25:33 +0330

    chore: conf for server

--- CoffeeShopWebsite/config/settings.py
+++ CoffeeShopWebsite/config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -155,9 +156,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -181,3 +183,5 @@ ALLOWED_HOSTS = ["*"]
 # crispy form
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- CoffeeShopWebsite/config/wsgi.py
+++ CoffeeShopWebsite/config/wsgi.py
@@ -12,7 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
 app = application
