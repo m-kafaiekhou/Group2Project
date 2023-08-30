005420d2486b098129699aeb09a545b8bf350d73
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-08-30 14:04:24 +0330

    test: define GalleryPageViewTestCase

--- CoffeeShopWebsite/coffeeshop/tests/test_views.py
+++ CoffeeShopWebsite/coffeeshop/tests/test_views.py
@@ -8,3 +8,11 @@ class HomePageViewTestCase(TestCase):
         response = self.client.get(url)
         self.assertIn("top_rated_items", response.context)
         self.assertIsNone(response.context["top_rated_items"])
+
+
+class GalleryPageViewTestCase(TestCase):
+    def test_gallery_page_context_data(self):
+        url = reverse("coffeeshop:gallery")
+        response = self.client.get(url)
+        self.assertIn("page_name", response.context)
+        self.assertEqual(response.context["page_name"], "gallery")
